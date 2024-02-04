17238ee05563aed812e51a658e681fd934200184
mahaa <mahaa@MAHA>
2024-02-04 13:25:07 +0200

    all cases for triangle

--- Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
+++ Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
@@ -13,26 +13,79 @@ public class AppTest {
 	TriangleClassifier triangle = new TriangleClassifier();
 	
 	@Test
-  
+  // Test for Equilateral triangle
 	public void equilateralTriangle(){
-        int a = 2;
-        int b = 2;
-        int c=2;
+        int a = 10;
+        int b = 10;
+        int c = 10;
         triangle.classifyTriangle(a,b,c);
-        String expected =Triangle_Types.isosceles.toString();
+        String expected =Triangle_Types.equilateral.toString();
         String result = triangle.get_type().toString();
         assertEquals(expected, result.toString());
     }
+	
+	//// Test  for scalene Triangle 
 	@Test
-	public void check_Scalene(){
-        int a = 2;
-        int b = 2;
-        int c=4;
+	public void scaleneTriangle(){
+        int a = 3;
+        int b = 4;
+        int c = 5;
         triangle.classifyTriangle(a,b,c);
-        String expected =Triangle_Types.Nottriangle.toString();
+        String expected =Triangle_Types.Scalene.toString();
         String result = triangle.get_type().toString();
-        assertEquals(expected, result.toString());
-        
+        assertEquals(expected, result.toString());    
     }
-    /// need to solve the homework
+	
+	
+	
+	
+	/// Test for isoScales Triangle
+	@Test
+	public void isoScalesTriangle() {
+		int a = 5;
+		int b = 5;
+		int c = 3;
+		triangle.classifyTriangle(a, b, c);
+		String expected =Triangle_Types.isosceles.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected,result.toString());
+			
+	}
+	
+	
+	
+	
+	/// Test for false data (not a triangle)
+	@Test
+	public void notTraingle() {
+		int a = 1 ;
+		int b = 2 ;
+		int c = 8;
+		triangle.classifyTriangle(a, b, c);
+		String expected =Triangle_Types.Nottriangle.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+	
+	
+	
+	
+	// Test for an error input 
+	@Test 
+	public void input_IsError() {
+		int a = -3;
+		int b = -1;
+		int c = 6;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.ERROR.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+	
+	
+	
+	
+	
+	
+
 }  
